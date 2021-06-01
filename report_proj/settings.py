e2d93c33bf5ab06f01b0dcc9ad78b8b5e434a33d
sbshchen <shikun.chen@stud.uni-due.de>
2021-06-01 11:32:17 +0200

    project started

--- report_proj/settings.py
+++ report_proj/settings.py
@@ -84,9 +84,19 @@ WSGI_APPLICATION = 'report_proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
+    # PostgreSQL database connection
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': '1228',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
